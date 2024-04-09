4d3af904dff76d5564f02fcd5eddbc1043c7c724
ugo.benavides <ugo.benavides@gmail.com>
2024-04-09 14:56:35 +0200

    ups region and version

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,15 +1,15 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-resource "random_pet" "prefix" {}
+# resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.prefix.id}-rg"
-  location = "West US 2"
+  name     = "terra-rg"
+  location = "East US"
 
   tags = {
     environment = "Demo"
@@ -17,11 +17,11 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
-  location            = azurerm_resource_group.default.location
+  name                = "terra-aks"
+  location            = "East US"
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${random_pet.prefix.id}-k8s"
-  kubernetes_version  = "1.26.3"
+  dns_prefix          = "terra-k8s"
+  # kubernetes_version  = "1.26.3"
 
   default_node_pool {
     name            = "default"
